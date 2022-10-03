52bc0b5e2552945b7498fad740e29b1a9a23a124
Walter Garcia De Leon <wgarcia@evopointsolution.com>
2022-10-03 18:48:20 -0400

    App creada

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,61 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  nombre: string='';
+  fecha: Date;
+  grupoEtario: string = '';
+  edadPersona: any;
+
+  
+
+  constructor() {
+    
+  }
+
+  enviar(){
+    this.DefinidRangoEdades(this.nombre,this.fecha)
+  }
+
+  DefinidRangoEdades(_nombre: string, _fechaNacimiento: Date){
+
+    let fechaNacimiento = new Date(_fechaNacimiento)
+    let fechaHoy = new Date()
+
+    let edad = fechaHoy.getFullYear() - fechaNacimiento.getFullYear()
+    this.edadPersona = edad;
+
+    if (edad >= 0 && edad <=11){
+      this.grupoEtario = 'Niñez'
+    }
+
+    if (edad >= 0 && edad <=11){
+      this.grupoEtario = 'Niñez'
+    }
+
+    if(edad >11 && edad<= 18){
+      this.grupoEtario = 'Adolescencia'
+    }
+
+    if (edad >18 && edad <30){
+      this.grupoEtario = 'Juventud'
+    }
+
+    if (edad >=30 && edad <60){
+      this.grupoEtario = 'Adultez'
+    }
+
+    if (edad >= 60 ){
+      this.grupoEtario = 'Vejez'
+    }
+
+
+
+    console.log("DAtos")
+    console.log(_nombre, _fechaNacimiento)
+    console.log(edad);
+ 
+
+
+  }
 
 }
